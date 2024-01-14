b81b0ea2a3e558992cff91d56814c20fa29c7578
Pedro Rabitti <63475606+hortapimo@users.noreply.github.com>
2024-01-13 22:39:43 -0300

    aranco el primer motor

--- src/modelo.cpp
+++ src/modelo.cpp
@@ -2,6 +2,8 @@
 #include <cmath>
 
 #define PIN_M1 17
+#define PIN_M1_DIR1 18
+#define PIN_M1_DIR2 19
 #define PIN_M2 22
 #define PIN_M3 32
 #define PIN_M4 13
@@ -11,7 +13,7 @@
 #define CH_M3 13
 #define CH_M4 12
 
-#define FREC 10000 //en hz
+#define FREC 1000 //en hz
 #define RES 12 //bits
 
 #define FALLO 0
@@ -23,21 +25,29 @@ int dutyCicle = maxRes/2;
 
 void iniciarPines()
 {
-ledcSetup(CH_M1, FREC, RES);
-ledcSetup(CH_M2, FREC, RES);
-ledcSetup(CH_M3, FREC, RES);
-ledcSetup(CH_M4, FREC, RES);
+//Pines direccion
+    pinMode(PIN_M1_DIR1, OUTPUT);
+    pinMode(PIN_M1_DIR2, OUTPUT);
+    //inicio direccion de las velocidades del motor
+    digitalWrite(PIN_M1_DIR1, HIGH);
+    digitalWrite(PIN_M1_DIR2, LOW);
+
+//setup pines vel motores
+    ledcSetup(CH_M1, FREC, RES);
+    ledcSetup(CH_M2, FREC, RES);
+    ledcSetup(CH_M3, FREC, RES);
+    ledcSetup(CH_M4, FREC, RES);
 
 
-ledcAttachPin(PIN_M1,CH_M1);//15 se refiere al canal que se va atar
-ledcAttachPin(PIN_M2,CH_M2);
-ledcAttachPin(PIN_M3,CH_M3);
-ledcAttachPin(PIN_M4,CH_M4);
-
-ledcWrite(CH_M1,dutyCicle);
-ledcWrite(CH_M2,dutyCicle);
-ledcWrite(CH_M3,dutyCicle);
-ledcWrite(CH_M4,dutyCicle);
+    ledcAttachPin(PIN_M1,CH_M1);//15 se refiere al canal que se va atar
+    ledcAttachPin(PIN_M2,CH_M2);
+    ledcAttachPin(PIN_M3,CH_M3);
+    ledcAttachPin(PIN_M4,CH_M4);
+    //arrancan al 50 % los motores
+    ledcWrite(CH_M1,dutyCicle);
+    ledcWrite(CH_M2,dutyCicle);
+    ledcWrite(CH_M3,dutyCicle);
+    ledcWrite(CH_M4,dutyCicle);
 
 }
 
